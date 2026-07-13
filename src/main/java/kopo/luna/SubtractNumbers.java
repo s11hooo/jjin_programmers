9020239ccfe43163ebbc28baacabf63bcbbd899b
hakgyogyojangchitsoldodook <designkim06@gmail.com>
2026-07-13 18:45:25 +0900

    두 수의 차 구하기

---
+++ src/main/java/kopo/luna/SubtractNumbers.java
@@ -0,0 +1,17 @@
+package kopo.luna;
+
+public class SubtractNumbers {
+    public static void main(String[] args) {
+
+        /*
+        정수 num1과 num2가 주어질 때,
+        num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+         */
+
+        System.out.println(solution(3, 2));
+    }
+    static int solution(int num1, int num2) {
+        int answer = num1 - num2;
+            return answer;
+    }
+}
